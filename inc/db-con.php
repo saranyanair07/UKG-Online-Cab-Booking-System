e90858209cd12234dc3a4309bb6195d12a37bbc4
saranyanair07 <127438496+saranyanair07@users.noreply.github.com>
2023-12-12 03:26:13 +0530

    Update db-con.php

--- inc/db-con.php
+++ inc/db-con.php
@@ -2,7 +2,7 @@
 define("MYSQL_SERVER", "cloudneethu.mysql.database.azure.com");
 define("MYSQL_USER", "cloudneethu");
 define("MYSQL_PASSWORD", "Krishna2002#");
-define("MYSQL_DATABASE", "cloudneethu");
+define("MYSQL_DATABASE", "dukgcab");
 
 $mysqli = new mysqli(MYSQL_SERVER,MYSQL_USER,MYSQL_PASSWORD) or die ('I cannot connect to the database because 1: ' . $mysqli->error);
 
